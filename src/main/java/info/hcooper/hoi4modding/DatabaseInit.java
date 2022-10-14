03b55766e4bb0b307b152ca6e8b18c23e633b702
hcooper <hcooper@dstl.gov.uk>
2022-10-14 21:58:01 +0100

    changing init to bean

--- src/main/java/info/hcooper/hoi4modding/DatabaseInit.java
+++ src/main/java/info/hcooper/hoi4modding/DatabaseInit.java
@@ -1,28 +1,26 @@
 package info.hcooper.hoi4modding;
 
 import info.hcooper.hoi4modding.ingest.IngestService;
-import info.hcooper.hoi4modding.ingest.NestedLevel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
 
-@Component
+@Configuration
 public class DatabaseInit {
 
     @Autowired
     IngestService ingestService;
 
-    @EventListener
-    public void onApplicationEvent(ContextRefreshedEvent event) throws IOException {
+    @Bean
+    void initialise() throws IOException {
 
-        NestedLevel buildingsFile = ingestService.readFile(new ClassPathResource("hoi/common/buildings/00_buildings.txt").getFile());
-        ingestService.processNestedLevel(buildingsFile);
+        //NestedLevel buildingsFile = ingestService.readFile(new ClassPathResource("hoi/common/buildings/00_buildings.txt").getFile());
+        //ingestService.processNestedLevel(buildingsFile);
         //ingestService.readFile(new ClassPathResource("hoi/common/characters/ANG.txt").getFile());
         //ingestService.readFile(new ClassPathResource("hoi/common/continuous_focus/generic.txt").getFile());
 
     }
+
 }
